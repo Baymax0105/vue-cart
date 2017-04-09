aba963657fea00320df16389dceac3524408e2d1
xiaobai Lee <xiaobai Lee>
2017-04-09 20:36:39 +0800

    vue-cart

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import goods from 'components/goods/goods'
+import vueTap from 'v-tap'
+import fastclick from 'fastclick'
+import Vuex from 'vuex'
+
+Vue.use(vueTap)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  // 添加的商品元素
+  addCartEl: {},
+  mutations: {
+    increment(state) {
+      state.count++
+    }
+  }
+})
+const router = new VueRouter({
+  routes: [{
+    path: '/goods',
+    component: goods
+  }],
+  linkActiveClass: 'active'
+})
+
+new Vue({
+  router,
+  store,
+  template: '<App/>',
+  components: {
+    App
+  },
+  data: {
+    eventHub: new Vue()
+  }
+}).$mount('#app')
+
+router.push('goods')
